922bf74fa90eaeba2791ef560fa8afbbd6264d88
DuarteProg <lucas.duartebarbosa@outlook.com>
2023-07-22 14:31:11 -0300

    repository created without database so far

---
+++ src/main/java/com/example/primeiroexemplo/repository/ProductRepository.java
@@ -0,0 +1,53 @@
+package com.example.primeiroexemplo.repository;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.primeiroexemplo.model.Product;
+
+@Repository
+public class ProductRepository {
+
+    private List<Product> products = new ArrayList<Product>();
+    private Integer lastId = 0;
+
+    public List<Product> getAll() {
+        return products;
+    }
+
+    public Optional<Product> getById(Integer id) {
+        return products.stream()
+                .filter(product -> product.getId() == id)
+                .findFirst();
+    }
+
+    public Product add(Product product) {
+        ++lastId;
+
+        product.setId(lastId);
+        products.add(product);
+
+        return product;
+    }
+
+    public void delete(Integer id) {
+        products.removeIf(product -> product.getId() == id);
+    }
+
+    public Product update(Product product) {
+        Optional<Product> productFound = getById(product.getId());
+        if (productFound.isEmpty()) {
+            throw new InputMismatchException("Product Not Found");
+        }
+
+        delete(product.getId());
+        products.add(product);
+
+        return product;
+    }
+
+}
